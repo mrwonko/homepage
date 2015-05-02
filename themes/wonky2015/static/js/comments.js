b6888b7d5de0313d77e9e65a5a7558fbbfb2a538
Willi Schinmeyer <willi@schinmeyer.de>
2015-05-02 19:25:27 +0200

    Finished comment frontend

--- themes/wonky2015/static/js/comments.js
+++ themes/wonky2015/static/js/comments.js
@@ -3,14 +3,14 @@
 /// <reference path="../../definitelytyped/lodash/lodash.d.ts" />
 
 // second parameter is list of required modules, omitting it turns this into a module lookup instead of a definition.
-angular.module( 'commentsApp', [ 'lodash', 'ngSanitize', 'backend' ] )
-.controller( 'CommentListCtrl', function( $scope, _, backend, post ) {
+angular.module( 'commentsApp', [ 'lodash', 'ngSanitize', 'templateGenerated' ] )
+.controller( 'CommentListCtrl', function( $scope, _, backendUrl, $http, post ) {
     ///<summary>Controller for list of comments</summary>
     ///<param name="backend" type="string"></param>
     $scope.comments = [];
     $scope.loading = true;
     $scope.error = null;
-    backend.getComments( post )
+    $http.get( backendUrl + "/rest/blog/" + post + "/comments" )
     .success( function( data, status, headers, config ) {
             $scope.loading = false;
             if( headers( "Content-Type" ) != "application/json" ) {
@@ -27,4 +27,34 @@ angular.module( 'commentsApp', [ 'lodash', 'ngSanitize', 'backend' ] )
             $scope.error = status == 0 ? 'Connection refused.' : data;
     } );
 } )
+.controller( 'newCommentCtrl', function( $scope, backendUrl, $http, post ) {
+    $scope.comment = {
+        "author": "",
+        "content": "",
+        "email": "",
+        "url": ""
+    };
+    $scope.loading = false;
+    $scope.error = null;
+    $scope.submitted = false;
+    $scope.submit = function() {
+        $scope.loading = true;
+        $http.post( backendUrl + "/rest/blog/" + post + "/comments", $scope.comment )
+        .success( function( data, status, headers, config ) {
+            $scope.loading = false;
+            if( data.spam ) {
+                $scope.error = "You appear to be a spammer. Boo! Boo!";
+            } else {
+                $scope.submitted = true;
+            }
+        })
+        .error( function( data, status, headers, config ) {
+            $scope.loading = false;
+            $scope.error = status == 0 ? 'Connection refused.' : data;
+        } );
+    };
+    $scope.dismissError = function() {
+        $scope.error = null;
+    };
+} );
 ;
