167df1313c105f1c53f8aa74da43ef887628012f
Willi Schinmeyer <willi@schinmeyer.de>
2015-05-04 00:19:20 +0200

    updated update.sh for latest server requirements

--- update.sh
+++ update.sh
@@ -1,2 +1,6 @@
-git pull && pelican 2> error.log
-
+git pull && echo "pulled latest revision"
+../bin/pelican 2>error.log && echo "ran pelican"
+if [ -e "backend/backend.pid" ]
+then
+  kill -INT `cat backend/backend.pid` && echo "killed backend"
+fi
