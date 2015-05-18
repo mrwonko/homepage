c35f9f2403ae7fb9ffdea91e670ac7850557fc9a
Willi Schinmeyer <willi@schinmeyer.de>
2015-05-18 14:01:23 +0200

    update script now works on separate branch to keep local changes to config etc.

--- update.sh
+++ update.sh
@@ -1,4 +1,8 @@
-git pull && echo "pulled latest revision"
+git checkout master && \
+ git pull && \
+ git checkout production && \
+ git merge master -m "merge changes" && \
+ echo "pulled latest revision"
 ../bin/pelican 2>error.log && echo "ran pelican"
 if [ -e "backend/backend.pid" ]
 then
