75d0fba05f5b6a23b9c403e790f5f62c548a9502
Willi Schinmeyer <willi@schinmeyer.de>
2016-04-02 13:30:25 +0200

    cleaner update script

--- update.sh
+++ update.sh
@@ -1,9 +1,19 @@
-git checkout master && \
- git pull && \
- git checkout production && \
- git merge master -m "merge changes" && \
- echo "pulled latest revision"
+#!/bin/sh
+
+# exit on error
+set -e
+
+# fetch latest version
+git checkout master
+git pull
+git checkout production
+git merge master -m "merge changes"
+echo "pulled latest revision"
+
+# run pelican
 ../bin/pelican 2>error.log && echo "ran pelican"
+
+# kill backend (will be restarted by systemd)
 if [ -e "backend/backend.pid" ]
 then
   kill -INT `cat backend/backend.pid` && echo "killed backend"
